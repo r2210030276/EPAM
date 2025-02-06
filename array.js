0bb8c0d64ee073530634d8408ff5129ca96ebe1b
Ravi Teja <parsharaviteja7@gmail.com>
2025-02-06 13:55:39 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,12 @@
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits[0]);
+
+fruits.push("Mango");
+console.log(fruits); 
+
+fruits.pop();
+console.log(fruits); 
+
+fruits.forEach(fruit => console.log(fruit));
+
+console.log(fruits.includes("Banana")); 
